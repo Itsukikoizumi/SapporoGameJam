4f8e5420bb0fc9d9d4cc9b232da673b2414d6c09
maruyakouhei03 <maruyakouhei03@gmail.com>
2025-08-04 16:50:49 +0900

    セーフゾーン完成

---
+++ 202408GameInSchool-fullExtract/Assets/1_CoreForGameJam/Scripts/Scene/ClearScene.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Hellmade.Sound;
+using UnityEngine;
+
+public class ClearScene : SceneBase
+{
+    /// <summary>
+    /// ゲーム開始時に画面を隠す処理
+    /// </summary>
+    public override void OnGameStart()
+    {
+        Hide();
+    }
+
+    /// <summary>
+    /// シーンへの遷移タイミング時の処理
+    /// </summary>
+    public override void OnSceneStart()
+    {
+        Show();
+        Debug.Log("タイトルシーン開始");
+
+        EazySoundManager.StopAllMusic();
+        EazySoundManager.PlayMusic(AudioLoader.BGM("魔法使いの旅路"));
+    }
+
+    /// <summary>
+    /// SceneManager側でフレーム毎に呼ばれる処理
+    /// </summary>
+    public override void OnUpdate()
+    {
+    }
+
+    /// <summary>
+    /// シーンから離れる時の処理
+    /// </summary>
+    public override void OnSceneEnd()
+    {
+        Hide();
+    }
+
+    public void OnNextScene()
+    {
+        EazySoundManager.PlayUISound(AudioLoader.SE("Accept"));
+        SceneManager.Instance.ChangeSceneWithFade(2);
+        EazySoundManager.StopAllMusic();
+    }
+
+    // private
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ 202408GameInSchool-fullExtract/Assets/SafeZpne.cs
@@ -0,0 +1,37 @@
+﻿using Surviver;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class SafeZpne : MonoBehaviour
+{
+    /// <summary>
+    /// 有効時間、単位は秒
+    /// </summary>
+    [SerializeField] private float Sec = 10f;
+
+    [SerializeField] private string nextSceneName;
+    [SerializeField] private int nextSceneIndex = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerCharacter"))
+        {
+            SceneManager.Instance.OnNextScene();
+        }
+    }
+}   
+
+ 
